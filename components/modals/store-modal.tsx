743d7157f1c6c6bd91ef103927e4ba284217a8ac
abderrahimelamarty <abdrahim.tawnat12345@gmail.com>
2023-07-17 12:20:34 +0100

    set up the environment

---
+++ components/modals/store-modal.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+import { Modal } from "@/components/ui/modal";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+
+
+
+export const StoreModal = () => {
+  const storeModal = useStoreModal();
+
+
+
+
+
+
+  return (
+    <Modal
+      title="Create store"
+      description="Add a new store to manage products and categories."
+      isOpen={storeModal.isOpen} 
+      onClose={storeModal.onClose}
+    >
+      Create store
+    </Modal>
+  );
+};
