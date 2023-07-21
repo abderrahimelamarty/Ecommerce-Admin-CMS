2ed84738578ee7f21e6fd81947b1ee2f25ef313a
abderrahimelamarty <abdrahim.tawnat12345@gmail.com>
2023-07-21 12:31:13 +0100

    add-navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,6 +1,7 @@
 import { UserButton, auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
+import StoreSwitcher from "@/components/store-switcher";
 import { MainNav } from "@/components/main-nav";
 import prismadb from "@/lib/prismadb";
 
@@ -20,10 +21,10 @@ const Navbar = async () => {
   return ( 
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
-      
+        <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
-       
+          
           <UserButton afterSignOutUrl="/" />
         </div>
       </div>
