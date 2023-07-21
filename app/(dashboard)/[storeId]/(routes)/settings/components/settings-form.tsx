2b646738781d0463824b55099102f12daa08251c
abderrahimelamarty <abdrahim.tawnat12345@gmail.com>
2023-07-21 12:33:18 +0100

    crud-store

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -72,7 +72,7 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
       router.push('/');
       toast.success('Store deleted.');
     } catch (error: any) {
-      toast.error('Make sure you removed all products and categories first.');
+      toast.error('Make sure you removed all products and categories first ');
     } finally {
       setLoading(false);
       setOpen(false);
