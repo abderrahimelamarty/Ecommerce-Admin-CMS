11a01164842c5d45cc7d0f8663fcdce276c5e048
abderrahimelamarty <abdrahim.tawnat12345@gmail.com>
2023-07-24 09:46:18 +0100

    colors-crud

--- app/(dashboard)/[storeId]/(routes)/colors/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/colors/page.tsx
@@ -2,15 +2,15 @@ import { format } from "date-fns";
 
 import prismadb from "@/lib/prismadb";
 
-import { BillboardColumn } from "./components/columns"
-import { BillboardClient } from "./components/client";
+import { ColorColumn } from "./components/columns"
+import { ColorClient } from "./components/client";
 
-const BillboardsPage = async ({
+const ColorsPage = async ({
   params
 }: {
   params: { storeId: string }
 }) => {
-  const billboards = await prismadb.billboard.findMany({
+  const colors = await prismadb.color.findMany({
     where: {
       storeId: params.storeId
     },
@@ -19,19 +19,20 @@ const BillboardsPage = async ({
     }
   });
 
-  const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+  const formattedColors: ColorColumn[] = colors.map((item) => ({
     id: item.id,
-    label: item.label,
+    name: item.name,
+    value: item.value,
     createdAt: format(item.createdAt, 'MMMM do, yyyy'),
   }));
 
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient data={formattedBillboards} />
+        <ColorClient data={formattedColors} />
       </div>
     </div>
   );
 };
 
-export default BillboardsPage;
+export default ColorsPage;
